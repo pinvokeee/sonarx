bf7937426d996e37f1d7344a9db319139d74cae1
pinvokeee <nanasisine+1@gmail.com>
2023-10-03 07:21:42 +0900

    test

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { makeOffline } from "vite-plugin-make-offline";
-
-export default defineConfig({
-  plugins: [react(), makeOffline()],
-})
